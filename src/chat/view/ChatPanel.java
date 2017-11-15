ec1956473cd5899b88ce8bc507d9b68d000c941b
tankedshoe <tankedshoe@gmail.com>
2017-11-15 09:03:02 -0700

    Added some methods and pre-made them if that makes any sense. (Did this to get rid of errors)

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,6 +1,46 @@
 package chat.view;
 
-public class ChatPanel
-{
+import chat.controller.ChatbotController;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.JButton;
 
+public class ChatPanel extends JPanel
+{
+	private ChatbotController appController;
+	private JTextArea responseArea;
+	private JTextField input;
+	private JButton submit;
+	private SpringLayout layout;
+	
+	public ChatPanel() 
+	{
+		super();
+		appController = new ChatbotController();
+		responseArea = new JTextArea();
+		input = new JTextField();
+		submit = new JButton();
+		layout = new SpringLayout();
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	public void setupPanel()
+	{
+		
+	}
+	
+	public void setupLayout()
+	{
+		
+	}
+	
+	public void setupListeners()
+	{
+		
+	}
 }
