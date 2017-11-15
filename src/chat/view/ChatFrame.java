557e432eeaa06eebb4a6edfec7ba681e0d7b4f36
tankedshoe <tankedshoe@gmail.com>
2017-11-15 08:41:45 -0700

    Developed the ChatFrame a tad bit more.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -6,17 +6,22 @@ import javax.swing.JFrame;
 public class ChatFrame extends JFrame
 {
 	private ChatbotController appController;
+//	private ChatPanel appPanel;
 	
 	public ChatFrame(ChatbotController appController)
 	{
 		super();
 		this.appController = appController;
-		
+//		appPanel = new ChatPanel(appController);
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
+		this.setSize(500, 500);
+		this.setTitle("ChattaBotta");
+//		this.setContentPane(appPanel);
+		this.setResizable(false);
 		this.setVisible(true);
 	}
 }
