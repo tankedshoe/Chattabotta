09d9bc6dbc6bcc5a606d1583b99d81163c5ab6dc
tankedshoe <tankedshoe@gmail.com>
2017-12-04 09:01:04 -0700

    Developed the HTML checker a lil bit.

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -186,7 +186,40 @@ public class Chatbot
 	
 	public boolean htmlTagChecker(String input)
 	{
-		return false;
+		boolean containsHTML = false;
+		
+		if(input == null || !input.contains("<"))
+		{
+			return containsHTML;
+		}
+		
+		int firstOpen = input.indexOf("<");
+		int firstClose = input.indexOf(">", firstOpen);
+		int secondOpen = 9;
+		int secondClose = -9;
+		String tagText = "";
+		
+		//Check bad tags
+		if(input.contains("<>") || input.indexOf("< >") > -1)
+		{
+			containsHTML = false;
+		}
+		
+		//Check singleton
+		if(input.toUpperCase().contains("<P>") || input.toLowerCase().contains("<br>"))
+		{
+			containsHTML = true;
+		}
+		
+		//Check others
+		else if(firstClose > firstOpen)
+		{
+			//Others
+			tagText = input.substring(firstOpen + 1, firstClose).toLowerCase();
+			secondOpen = input.toLowerCase().indexOf("</" + tagText, firstClose);
+		}
+		
+		return containsHTML;
 	}
 	
 	public boolean userNameChecker(String input)
